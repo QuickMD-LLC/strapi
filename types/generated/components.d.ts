913ff7007acf150011e764ac12ef5d51c6578d7e
Sam Bernard <sambernard@users.noreply.github.com>
2024-12-06 13:47:09 -0700

    remove redundent select option for frequency

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -24,7 +24,7 @@ export interface PrescriptionPrescriptionOption extends Struct.ComponentSchema {
       Schema.Attribute.DefaultTo<'Solution Pen-injector'>;
     drug: Schema.Attribute.Relation<'oneToOne', 'api::drug.drug'>;
     durationDays: Schema.Attribute.Integer & Schema.Attribute.Required;
-    frequency: Schema.Attribute.Enumeration<['weekly', 'qw']> &
+    frequency: Schema.Attribute.Enumeration<['qw']> &
       Schema.Attribute.Required &
       Schema.Attribute.DefaultTo<'weekly'>;
     name: Schema.Attribute.String &
